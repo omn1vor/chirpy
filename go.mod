7fcf4adb397b5017e9cfbfc558868695335a75a3
Daniel Pimenov <daniil.pimenov@gmail.com>
2023-10-11 18:25:52 +0400

    Add fileserver with static index and logo

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/omn1vor/chirpy
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	corsMux := middlewareCors(mux)
+
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: corsMux,
+	}
+
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
+	log.Println("Starting server on port", port)
+	log.Fatal(server.ListenAndServe())
+}
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
